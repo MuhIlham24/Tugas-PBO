37b84d3b16b937bd77c9c72f8eb3f7947217de74
MuhIlham24 <114120892+MuhIlham24@users.noreply.github.com>
2024-03-13 15:19:20 +0800

    Update If2.java

--- Tugas 3/Tugas3_SourceCode_Praktek/If2.java
+++ Tugas 3/Tugas3_SourceCode_Praktek/If2.java
@@ -1,7 +1,7 @@
 /*  NIM: 13020220041
     Nama: Muh Ilham Nur Hidayat Akbar
     Hari/Tgl: Selasa/5-03-2024
-    Waktu Pengerjaan = 13.40 - 15.00
+    Waktu Pengerjaan = 13.40 - 17.00
 */
 
 import java.util.Scanner;
